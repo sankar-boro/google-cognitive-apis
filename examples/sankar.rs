be9c20449f3dd0cba8b8ac743877d40a214912e1
Sankar Boro <sankar.boro@yahoo.com>
2024-10-31 09:43:16 +0530

    v2 example for StreamingRecognitionConfig

--- examples/sankar.rs
+++ examples/sankar.rs
@@ -1,12 +1,14 @@
-use google_cognitive_apis::api::grpc::google::cloud::speechtotext::v1::{
-    recognition_config::AudioEncoding, RecognitionConfig, StreamingRecognitionConfig,
+use google_cognitive_apis::api::grpc::google::cloud::speechtotext::v2::{
+    explicit_decoding_config::AudioEncoding, recognition_config::DecodingConfig,
+    ExplicitDecodingConfig, RecognitionConfig, RecognitionFeatures, SpeechAdaptation,
+    StreamingRecognitionConfig, StreamingRecognitionFeatures, TranslationConfig,
 };
-use google_cognitive_apis::speechtotext::recognizer::Recognizer;
+use google_cognitive_apis::speechtotext::recognizer_v2::Recognizer;
 
 use log::*;
-use std::env;
 use std::fs::{self, File};
 use std::io::Read;
+use std::{env, vec};
 
 #[tokio::main]
 async fn main() {
@@ -17,23 +19,46 @@ async fn main() {
     let credentials = fs::read_to_string("/tmp/cred.json").unwrap();
     let streaming_config = StreamingRecognitionConfig {
         config: Some(RecognitionConfig {
-            encoding: AudioEncoding::Linear16 as i32,
-            sample_rate_hertz: 8000,
-            audio_channel_count: 1,
-            enable_separate_recognition_per_channel: false,
-            language_code: "en-US".to_string(),
-            max_alternatives: 1,
-            profanity_filter: false,
-            speech_contexts: vec![],
-            enable_word_time_offsets: false,
-            enable_automatic_punctuation: false,
-            diarization_config: None,
-            metadata: None,
+            // enable_separate_recognition_per_channel: false,
+            // language_code: "en-US".to_string(),
+            // speech_contexts: vec![],
+            // metadata: None,
+            // use_enhanced: false,
             model: "".to_string(),
-            use_enhanced: false,
+            language_codes: vec![],
+            features: Some(RecognitionFeatures {
+                profanity_filter: false,
+                enable_word_time_offsets: false,
+                enable_word_confidence: false,
+                enable_automatic_punctuation: false,
+                enable_spoken_punctuation: false,
+                enable_spoken_emojis: false,
+                multi_channel_mode: 2,
+                diarization_config: None,
+                max_alternatives: 1,
+            }),
+            adaptation: Some(SpeechAdaptation {
+                phrase_sets: vec![],
+                custom_classes: vec![],
+            }),
+            transcript_normalization: None,
+            translation_config: Some(TranslationConfig {
+                target_language: "es-ES".to_string(),
+            }),
+            decoding_config: Some(DecodingConfig::ExplicitDecodingConfig(
+                ExplicitDecodingConfig {
+                    encoding: AudioEncoding::Linear16 as i32,
+                    sample_rate_hertz: 8000,
+                    audio_channel_count: 1,
+                },
+            )),
         }),
-        single_utterance: false,
-        interim_results: true,
+        config_mask: None,
+        streaming_features: Some(StreamingRecognitionFeatures {
+            enable_voice_activity_events: false,
+            interim_results: true,
+            voice_activity_timeout: None,
+        }),
     };
 
     let mut recognizer =
